a18bcd439294b6f40bbf1c28c37258c200298f9a
Jeremias <Joelmata2@gmail.com>
2017-07-17 23:42:44 +0200

    installation of apollo packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ListDetailComponent } from './components/list-detail.page';
 import { UserDetailComponent } from './components/user-detail.page';
+import { ListListComponent } from './components/list-list.page';
 import { UserListComponent } from './components/user-list.page';
 
 import { ListService } from './services/list.service';
@@ -14,6 +15,12 @@ import { UserService } from './services/user.service';
 
 import { UserRolesResolverService } from './services/roles-resolver.service';
 import { CheckboxListComponent } from './components/checkbox-list.component';
+
+import { ApolloModule } from 'apollo-angular';
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
 
 const appRoutes: Routes = [
   {
@@ -31,6 +38,10 @@ const appRoutes: Routes = [
     }
   },
   {
+    path: 'lists',
+    component: ListListComponent
+  },
+  {
     path: 'users',
     component: UserListComponent
   },
@@ -41,11 +52,30 @@ const appRoutes: Routes = [
   }
 ];
 
+// by default, this client will send queries to `/graphql` (relative to the URL of your app)
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:8080'
+  }),
+});
+
+export function provideClient(): ApolloClient {
+  return client;
+}
+
 @NgModule({
-  imports: [BrowserModule, HttpModule, ReactiveFormsModule, RouterModule.forRoot(appRoutes)],
-  declarations: [AppComponent, UserDetailComponent, UserListComponent, ListDetailComponent, CheckboxListComponent],
+  imports: [BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    ApolloModule.forRoot(provideClient)
+  ],
+  declarations: [AppComponent, UserDetailComponent, UserListComponent, ListListComponent, ListDetailComponent, CheckboxListComponent],
   providers: [UserService, ListService, UserRolesResolverService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ src/app/components/list-list.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ListService, List } from '../services/list.service';
+import 'rxjs/add/operator/finally';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'if-list-list',
+  templateUrl: './list-list.page.html'
+})
+export class ListListComponent implements OnInit {
+  lists: Observable<List[]>;
+  isLoading = false;
+
+  constructor(private listService: ListService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.getLists();
+  }
+
+  getLists() {
+    this.isLoading = true;
+    this.lists = this.listService.getLists().finally(() => this.isLoading = false);
+  }
+}
